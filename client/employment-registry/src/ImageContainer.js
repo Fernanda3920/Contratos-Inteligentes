5e53ae4f63eec24aaa5129070bf4f602e77620a6
Fernanda3920 <fernandanevarez7171@gmail.com>
2025-01-08 01:00:35 -0800

    Informacion

---
+++ client/employment-registry/src/ImageContainer.js
@@ -0,0 +1,24 @@
+/*import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css'; // Puedes agregar estilos personalizados aquí
+
+const ImageContainer = ({ src, alt, style, className }) => {
+  return (
+    <div className={`image-container ${className}`} style={style}>
+      <img src={src} alt={alt} className="image" />
+      <div className='container'> 
+        <h1> ALTA DE REGISTROS </h1>
+        <p>
+          Gereración de contrato digital.
+        </p>
+
+        <button>Dar de alta un nuevo empleado</button>  
+        <button>Recuperar datos de un empleado</button>  
+      </div>
+    </div>
+    
+  );
+};
+
+
+export default ImageContainer;*/
